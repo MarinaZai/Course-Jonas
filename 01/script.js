6d724f00f078fb306f5def45dcfa7bf283ae3b38
MarinaZai <marintos34@gmail.com>
2023-07-04 14:26:56 +0300

    feat:add coding challenge 2

--- 01/script.js
+++ 01/script.js
@@ -38,16 +38,35 @@ let markWeight = 78;
 let markHeight = 1.69;
 let johnWeight = 92;
 let johnHeight = 1.95;
-let markBMI = markWeight / markHeight ** 2;
-let johnBMI = johnWeight / johnHeight ** 2;
+let markBMI = (markWeight / markHeight ** 2).toFixed(1);
+let johnBMI = (johnWeight / johnHeight ** 2).toFixed(1);
 let markHigherBMI = markBMI > johnBMI;
-console.log(markBMI,johnBMI,markHigherBMI)
+//console.log(markBMI, johnBMI, markHigherBMI);
+
+if (markBMI > johnBMI) {
+  console.log("Mark's BMI is higher than John's");
+  console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
+} else {
+  console.log("John's BMI is higher than Mark's");
+  console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+}
 
 let markWeightTwoEx = 95;
 let markHeightTwoEx = 1.88;
 let johnWeightTwoEx = 85;
 let johnHeightTwoEx = 1.76;
-let markBMITwoEx = markWeightTwoEx / markHeightTwoEx ** 2;
-let johnBMITwoEx = johnWeightTwoEx / johnHeightTwoEx ** 2;
+let markBMITwoEx = (markWeightTwoEx / markHeightTwoEx ** 2).toFixed(1);
+let johnBMITwoEx = (johnWeightTwoEx / johnHeightTwoEx ** 2).toFixed(1);
 let markHigherBMITwoEx = markBMITwoEx > johnBMITwoEx;
-console.log(markBMITwoEx,johnBMITwoEx,markHigherBMITwoEx)
+//console.log(markBMITwoEx, johnBMITwoEx, markHigherBMITwoEx);
+if (markBMITwoEx > johnBMITwoEx) {
+  console.log("Mark's BMI is higher than John's");
+  console.log(
+    `Mark's BMI ${markBMITwoEx} is higher than John's ${johnBMITwoEx}`
+  );
+} else {
+  console.log("John's BMI is higher than Mark's");
+  console.log(
+    `John's BMI ${johnBMITwoEx} is higher than Mark's ${markBMITwoEx}`
+  );
+}
