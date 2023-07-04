a68b0d81e378c72e72aa9260022385da8079cb6e
MarinaZai <marintos34@gmail.com>
2023-07-04 13:39:01 +0300

    feat:add coding challenge 1

---
+++ 01/script.js
@@ -0,0 +1,53 @@
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "nothing";
+
+let job1 = "programmer";
+let job2 = "nothing";
+
+console.log(myFirstJob);
+ */
+
+/* let javaScript = true;
+console.log(javaScript);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "ssss"); */
+
+//CODING CHALLENGE 1
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
+let markHigherBMI = markBMI > johnBMI;
+console.log(markBMI,johnBMI,markHigherBMI)
+
+let markWeightTwoEx = 95;
+let markHeightTwoEx = 1.88;
+let johnWeightTwoEx = 85;
+let johnHeightTwoEx = 1.76;
+let markBMITwoEx = markWeightTwoEx / markHeightTwoEx ** 2;
+let johnBMITwoEx = johnWeightTwoEx / johnHeightTwoEx ** 2;
+let markHigherBMITwoEx = markBMITwoEx > johnBMITwoEx;
+console.log(markBMITwoEx,johnBMITwoEx,markHigherBMITwoEx)
