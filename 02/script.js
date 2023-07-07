04ed555e8797271ca1964a94db01d30c001d9aba
MarinaZai <marintos34@gmail.com>
2023-07-07 12:10:28 +0300

    CODING CHALLENGE(02)-1

---
+++ 02/script.js
@@ -0,0 +1,21 @@
+//coding challenge(02)-1
+const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
+  return (scoreOne + scoreTwo + scoreThree) / 3;
+};
+
+const avgDolhins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+//const avgDolhins = calcAverage(85, 54, 41);
+//const avgKoalas = calcAverage(23, 34, 27);
+console.log(avgDolhins, avgKoalas);
+
+const checkWinner = function () {
+  if (avgDolhins >= 2 * avgKoalas) {
+    console.log(`Dolhins win (${avgDolhins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolhins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+  } else {
+    console.log("Nobody win");
+  }
+};
+checkWinner();
