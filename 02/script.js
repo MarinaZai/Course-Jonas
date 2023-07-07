d13f7cfff609c3dece1d8d25f13c005f6022da61
MarinaZai <marintos34@gmail.com>
2023-07-07 14:07:05 +0300

    feat:CODING CHALLENGE(02)-2

--- 02/script.js
+++ 02/script.js
@@ -1,5 +1,5 @@
 //CODING CHALLENGE(02)-1
-const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
+/* const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
   return (scoreOne + scoreTwo + scoreThree) / 3;
 };
 
@@ -18,4 +18,42 @@ const checkWinner = function () {
     console.log("Nobody win");
   }
 };
-checkWinner();
+checkWinner(); */
+
+//CODING CHALLENGE(02)-2
+const billsArray = [125, 555, 44];
+
+let bill = billsArray[0];
+let procent = bill >= 50 && bill <= 300 ? 15 : 20;
+let tipResult = (bill * procent) / 100;
+console.log(
+  `The bill was ${bill}, the tip was ${tipResult}, and the total value ${
+    bill + tipResult
+  }`
+);
+
+let billTwo = billsArray[1];
+let procentTwo = billTwo >= 50 && billTwo <= 300 ? 15 : 20;
+let tipResultTwo = (billTwo * procentTwo) / 100;
+console.log(
+  `The bill was ${billTwo}, the tip was ${tipResultTwo}, and the total value ${
+    billTwo + tipResultTwo
+  }`
+);
+
+let billThree = billsArray[2];
+let procentThree = billThree >= 50 && billThree <= 300 ? 15 : 20;
+let tipResultThree = (billThree * procentThree) / 100;
+console.log(
+  `The bill was ${billThree}, the tip was ${tipResultThree}, and the total value ${
+    billThree + tipResultThree
+  }`
+);
+
+const tipsArray = [tipResult, tipResultTwo, tipResultThree];
+const totalArray = [
+  billsArray[0] + tipsArray[0],
+  billsArray[1] + tipsArray[1],
+  billsArray[2] + tipsArray[2],
+];
+console.log(tipsArray, totalArray);
